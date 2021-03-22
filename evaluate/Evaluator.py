685d23e594317fab00667f7c43eba41d8a90dd11
Kevin Xu <53694018+KevinXu-01@users.noreply.github.com>
2021-03-22 14:18:26 +0800

    增加了3种评价方法
    但是目前只通过下标搜索，必须保证评价时是对整个测试集或从第一条开始测试，如果从中间的开始测试，则会出现数据问题（或许可以通过给出起始下标的方法来完成，待修改）

--- evaluate/Evaluator.py
+++ evaluate/Evaluator.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append("..")
+from IO import AnalysisJson
+import os
+
 class Evaluator:
     # TODO
     # 第一种评价算法，测试数据的全label匹配才认为成功
@@ -8,19 +13,47 @@ class Evaluator:
     # [{"rn":[]，"rv":[]，"on":[],"ov":[]}]
     # 分别对应原因中的核心名词，原因中的谓语或状态，结果中的.....四项
     def recordMatch(self, forecast_dic_list: list, test_dic_list: list) -> float:
-        pass
+        sum = 0
+        for i in range(0, len(forecast_dic_list)):
+            if forecast_dic_list[i] == test_dic_list[i]:
+                sum += 1
+        return sum / (len(forecast_dic_list))
 
     # 第二种评价算法，统计预测准确的label在总label数量中的占比
     # 参数,返回值同上
     def labelMatch(self, forecast_dic_list: list, test_dic_list: list) -> float:
-        pass
+        sum = 0
+        for i in range(0, len(forecast_dic_list)):
+            for j in range(0, 4):
+                if forecast_dic_list[i][j] == test_dic_list[i][j]:
+                    sum += 1
+        return sum / ((4 * len(forecast_dic_list)))
+
 
     # 第三种评价算法，分别统计4种label的预测准确率
     # 参数,返回值同上
     def kindsOfLabelMath(self, forecast_dic_list: list, test_dic_list: list) -> float:
-        pass
+        sum = [0, 0, 0, 0]
+        precision = []
+        for i in range(0, len(forecast_dic_list)):
+            for j in range(0, 4):
+                if j == 0 and forecast_dic_list[i][j] == test_dic_list[i][j]:
+                    sum[0]  += 1
+                elif j == 1 and forecast_dic_list[i][j] == test_dic_list[i][j]:
+                    sum[1] += 1
+                elif j == 2 and forecast_dic_list[i][j] == test_dic_list[i][j]:
+                    sum[2] += 1
+                else:
+                    sum[3] += 1
+        for i in range(0, 4):
+            precision.append(sum[i] / len(forecast_dic_list))
+        return precision
 
 
 if __name__ == "__main__":
-    print("在这里进行测试")
+    path = os.path.dirname(os.getcwd()) + "\\res\\5201-5600.json"
+    aj = AnalysisJson.AnalysisJson(path)
+    forecast_dic_list = [['续航时间', '过短', '使用体验', ''], ['中航飞机的筹划资产置换', '置换', '大飞机产业链', '走强'], ['信用风险事件', '置换', '整体利差', '走强'], ['仓位调整', '需要', '调整', '走强'], ['私募产品', '延期兑付', '消费者', '投诉']]
+    evaluator = Evaluator()
+    print(evaluator.kindsOfLabelMath(forecast_dic_list, aj.getAllValueToDicList()))
     pass
